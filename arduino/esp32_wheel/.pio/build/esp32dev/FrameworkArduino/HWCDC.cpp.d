0800a10204d294650880dd2d6be50a201c7b5274
Fernando Ortiz <fernandooxz1@gmail.com>
2026-09-19 09:55:25 -0300

    Old version with the basics

---
+++ arduino/esp32_wheel/.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
+ /home/fernando/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ /home/fernando/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/fernando/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ arduino/esp32_wheel/.pio/build/esp32dev/libad9/Adafruit NeoPixel/psoc6.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/libad9/Adafruit\ NeoPixel/psoc6.c.o: \
+ .pio/libdeps/esp32dev/Adafruit\ NeoPixel/psoc6.c
